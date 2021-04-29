a1592ad89e0961e754a37fe2492ea2cbd4012558
Wojciech Pszonak <wpszonak@future-processing.com>
2021-04-29 17:26:48 +0200

    Add API and Docker Compose support

---
+++ BlazorDemo/BlazorDemo.Web/Pages/FetchData.cs
@@ -0,0 +1,21 @@
+﻿using BlazorDemo.Models.Weather;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace BlazorDemo.Web.Pages
+{
+    public partial class FetchData
+    {
+        private WeatherForecast[] forecasts;
+
+        [Inject]
+        private HttpClient Http { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+        }
+    }
+}
